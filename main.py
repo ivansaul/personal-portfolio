46e83fe30ba3902c432af56119efa5274ae433ee
ivansaul <saul_2503@hotmail.com>
2023-06-03 14:06:56 -0500

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import toml
 from pprint import pprint
 from jinja2 import Template
+from datetime import datetime
 from jinja2 import Environment, FileSystemLoader
 
 class Portfolio:
@@ -8,6 +9,8 @@ class Portfolio:
         self.about_toml = "config/about.toml"
         self.projects_toml = "config/projects.toml"
         self.resume_toml = "config/resume.toml"
+        self.blog_toml = "config/blog.toml"
+
 
 
 
@@ -29,6 +32,12 @@ class Portfolio:
         return data
 
 
+    def format_date(self, date: str):
+        date_object = datetime.strptime(date, "%Y-%m-%d")
+        formatted_date = date_object.strftime("%b %d, %Y")
+        return formatted_date
+
+
     def about(self):
         return self.load_toml_file(self.about_toml)
 
@@ -48,6 +57,9 @@ class Portfolio:
         toml_cfg = self.load_toml_file(self.resume_toml)
         return toml_cfg
 
+    def blog(self):
+        return self.load_toml_file(self.blog_toml)
+
 
 if __name__ == "__main__":
 
@@ -56,9 +68,11 @@ if __name__ == "__main__":
     projects = portfolio.projects()
     categories = portfolio.categories()
     resume = portfolio.resume()
+    blog = portfolio.blog()
       
 
     env = Environment(loader=FileSystemLoader('jinja'))
+    env.filters['format_date'] = portfolio.format_date
 
     template = env.get_template('index.jinja')
 
@@ -67,6 +81,7 @@ if __name__ == "__main__":
         projects = projects,
         categories = categories,
         resume = resume,
+        blog = blog,
         )
 
     portfolio.write_file("index.html", html_render)
