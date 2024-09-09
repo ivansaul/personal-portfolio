88c4df8dab0d906b74595c8c0e354f03328164c7
Shahrukh Mansuri <mansurishahrukh007@gmail.com>
2024-09-09 12:37:39 +0530

    Separate css component wise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,7 +36,7 @@ export class AppComponent implements OnInit {
   title = 'portfolio';
   tabOptions = inject(TAB_OPTIONS);
 
-  activeTab = this.tabOptions[1];
+  activeTab = this.tabOptions[2];
 
   ngOnInit(): void {}
 }
